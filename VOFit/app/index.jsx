c544ca19d7bb7292931aa1da80018f8219af3abe
OjSkillz <soojo06@gmail.com>
2025-09-06 07:17:58 +0100

    Added logo to home page

--- VOFit/app/index.jsx
+++ VOFit/app/index.jsx
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import Logo from '../assets/img/VOFit1.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Mental Health Care</Text>
-      <Text>For Everyone</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img}/>
+      <Text style={styles.title}>Fitness..For Voiceover Artistes!</Text>
+  
+    
 
     </View>
   )
@@ -15,6 +18,25 @@ export default Home
 
 const styles = StyleSheet.create({
     container: {
-        
-    }
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "rgba(255, 255, 255, 0.1)"
+    },
+    img: {
+      height: 200,
+      width: 200,
+      borderColor: 'rgba(25, 24, 94, 0.72)',
+      borderWidth: 0.5,
+      borderRadius: 10,
+      backgroundColor: "rgba(17, 2, 2, 0.1)"
+
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: '18',
+      fontStyle: 'italic',
+      marginTop: 20
+    },
+  
 })
